c391707b104a3f69191ee6ca7ad4b40ab3ae731b
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-11 01:48:36 +0530

    Modified to Validate Email Using Lambda

--- UserRegestrationUsingLambda/Program.cs
+++ UserRegestrationUsingLambda/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to User Registration Using Lambda Expression");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:First Name\n2:Check Last Name");
+            Console.WriteLine("1:First Name\n2:Check Last Name\n3:Check Email Id");
             int option = Convert.ToInt32(Console.ReadLine());
             UserRegistration userRegistration = new UserRegistration();
             switch (option)
@@ -16,6 +16,9 @@
                     break;
                 case 2:
                     userRegistration.CheckLastName("Divekar");
+                    break;
+                case 3:
+                    userRegistration.CheckEmailID("abc.xyz@bridgelabz.co.in");
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
