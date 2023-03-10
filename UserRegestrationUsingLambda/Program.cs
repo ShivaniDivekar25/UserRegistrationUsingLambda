770715831be569fcc9f4e6e4c0592baacbe3f775
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-11 02:11:34 +0530

    Modified to Validate Password Using Lambda

--- UserRegestrationUsingLambda/Program.cs
+++ UserRegestrationUsingLambda/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to User Registration Using Lambda Expression");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:First Name\n2:Check Last Name\n3:Check Email Id\n4:Check Phone Number\n5:Check Password1\n6:Check Password2\n7:Check Password3");
+            Console.WriteLine("1:First Name\n2:Check Last Name\n3:Check Email Id\n4:Check Phone Number\n5:Check Password1\n6:Check Password2\n7:Check Password3\n8:Check Password4");
             int option = Convert.ToInt32(Console.ReadLine());
             UserRegistration userRegistration = new UserRegistration();
             switch (option)
@@ -31,6 +31,9 @@
                     break;
                 case 7:
                     userRegistration.CheckPassword3("Shivani11");
+                    break;
+                case 8:
+                    userRegistration.CheckPassword4("Ashwini11@");
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
