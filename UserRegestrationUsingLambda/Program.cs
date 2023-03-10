77d625004982ce78e9268586b125637d2c1c2d99
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-11 01:27:33 +0530

    Added Welocome To User Registration Using Lambda Expression

---
+++ UserRegestrationUsingLambda/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegestrationUsingLambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Using Lambda Expression");
+        }
+    }
+}
